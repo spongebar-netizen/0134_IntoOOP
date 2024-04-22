9b0a27262c16046db48a93bd146067f749c2cbcd
spongebar-netizen <fatanjrz1@gmail.com>
2024-04-22 16:00:00 +0700

    void input

--- 0134_IntoOOP/part2.cpp
+++ 0134_IntoOOP/part2.cpp
@@ -23,3 +23,12 @@ private:
 	string namaMk;
 	int sks;
 public:
+
+	void input() {
+		cout << "Kode MK : ";
+		cin >> kodeMk;
+		cout << "Nama MK : ";
+		cin >> namaMk;
+		cout << "SKS : ";
+		cin >> sks;
+	}
