078123f0fa092535f2411c4a080a37fc7948d287
spongebar-netizen <fatanjrz1@gmail.com>
2024-04-22 16:01:01 +0700

    void output

--- 0134_IntoOOP/part2.cpp
+++ 0134_IntoOOP/part2.cpp
@@ -32,3 +32,9 @@ public:
 		cout << "SKS : ";
 		cin >> sks;
 	}
+
+	void output() {
+		cout << "Kode MK : " << kodeMk << endl;
+		cout << "Nama MK : " << namaMk << endl;
+		cout << "SKS : " << sks << endl;
+	}
