79d0f7aceff2b05122d1afcf7f0c11b05a5ebd97
spongebar-netizen <fatanjrz1@gmail.com>
2024-04-22 15:49:15 +0700

    void display

--- 0134_IntoOOP/0134_IntoOOP.cpp
+++ 0134_IntoOOP/0134_IntoOOP.cpp
@@ -20,3 +20,8 @@ public:
 		return panjang * lebar;
 	}
 
+	void display() {
+		cout << "Panjangnya" << panjang << endl;
+		cout << "Lebarnya" << lebar << endl;
+		cout << "Luasnya" << hitungLuas() << endl;
+	}
