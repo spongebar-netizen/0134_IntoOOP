df6ad2b48bed905746fcb3728002e21ff5b41ca5
spongebar-netizen <fatanjrz1@gmail.com>
2024-04-22 15:57:07 +0700

    void output

--- 0134_IntoOOP/part2.cpp
+++ 0134_IntoOOP/part2.cpp
@@ -8,4 +8,9 @@ public:
 	string nama;
 	int umur;
 	string jurusan;
+	void output() {
 
+		cout << "Nama : " << nama << endl;
+		cout << "Umur : " << umur << endl;
+		cout << "Jurusan : " << jurusan << endl;
+	}
